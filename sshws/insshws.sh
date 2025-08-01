853eb34d34fcd11e32c7aa728095a7250bacbcbb
PEYX <pxanime4@gmail.com>
2025-08-01 22:45:59 +0700

    Add files via upload

---
+++ sshws/insshws.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Proxy For Edukasi & Imclass
+file_path="/etc/handeling"
+
+# Cek apakah file ada
+if [ ! -f "$file_path" ]; then
+    # Jika file tidak ada, buat file dan isi dengan dua baris
+    echo -e "DevXwan Server Connected\nBLUE" | sudo tee "$file_path" > /dev/null
+    echo "File '$file_path' berhasil dibuat."
+else
+    # Jika file ada, cek apakah isinya kosong
+    if [ ! -s "$file_path" ]; then
+        # Jika file ada tetapi kosong, isi dengan dua baris
+        echo -e "DevXwan Server Connected\nBlue" | sudo tee "$file_path" > /dev/null
+        echo "File '$file_path' kosong dan telah diisi."
+    else
+        # Jika file ada dan berisi data, tidak lakukan apapun
+        echo "File '$file_path' sudah ada dan berisi data."
+    fi
+fi
+# Link Hosting Kalian
+sudo apt install python3
+
+wget -O /usr/local/bin/ws "http://myrid.my.id/os/sshws/ws"
+chmod +x /usr/local/bin/ws
+
+# Installing Service
+cat > /etc/systemd/system/ws.service << END
+[Unit]
+Description=Proxy Mod By Newbie Store 
+Documentation=https://t.me/newbie_store24
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/ws
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws.service
+systemctl start ws.service
+systemctl restart ws.service
+
+wget -O /usr/local/bin/ws-ovpn "http://myrid.my.id/os/sshws/ws"
+chmod +x /usr/local/bin/ws-ovpn
+
+# Installing Service
+cat > /etc/systemd/system/ws-ovpn.service << END
+[Unit]
+Description=Proxy Mod By NEWBIE STORE
+Documentation=https://t.me/newbie_store24
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/ws-ovpn 2086
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-ovpn
+systemctl restart ws-ovpn
+
